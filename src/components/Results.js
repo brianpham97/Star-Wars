70aad4ba4f1ecaa87c41e8964884a0c1357822e8
Brian Pham <phambri97@gmail.com>
2023-02-06 21:09:53 -0800

    Adding scroll effect

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { compare } from "./helpers/compare";
 import { reset } from "../features/selected";
 import ClipLoader from "react-spinners/ClipLoader"
@@ -9,6 +9,7 @@ const Results = () => {
   const [description, setDescription] = useState('')
   const [errorSubmit, setErrorSubmit] = useState(false)
   const [loader, setLoader] = useState(false)
+  const ref = useRef(null)
 
   const selectedCharacters = useSelector((state) => state.selected.value);
   const movies = useSelector((state) => state.films.value);
@@ -31,6 +32,7 @@ const Results = () => {
   const submit = async () => {
     if (selectedCharacters.left && selectedCharacters.right) {
       setDescription('');
+      ref.current.scrollIntoView({behavior: 'smooth'})
       runSpinner();
 
     } else {
@@ -50,18 +52,22 @@ const Results = () => {
     <div>
       <div className="w-screen-md mx-auto h-32 flex flex-col justify-center items-center relative">
         {errorSubmit ? (
-          <p id="fade-text" className="absolute top-0 text-red-500 text-center italic">
+          <p
+            id="fade-text"
+            className="absolute top-0 text-red-500 text-center italic"
+          >
             * Must select two characters *
           </p>
         ) : null}
         <div className="flex justify-between w-1/2">
-          <h1
-            id="button"
-            className="btn font-button bg-green-400 border-green-600"
-            onClick={submit}
-          >
-            Submit
-          </h1>
+            <h1
+              id="button"
+              className="btn font-button bg-green-400 border-green-600"
+              onClick={submit}
+            >
+              Submit
+            </h1>
+
           <h1
             id="button"
             className="btn font-button bg-red-400 border-red-600"
@@ -72,9 +78,11 @@ const Results = () => {
         </div>
       </div>
 
-      <div className="max-w-screen-md mx-auto h-28 flex justify-center bg-gradient-to-b from-gray-600 to-gray-800">
+      <div ref={ref} className="max-w-screen-md mx-auto h-28 flex justify-center bg-gradient-to-b from-gray-600 to-gray-800">
         {loader ? spinner : null}
-        <p id="fin" className="text-white text-lg mt-4 mx-3">{description}</p>
+        <p id="fin" className="text-white text-lg mt-4 mx-3">
+          {description}
+        </p>
       </div>
     </div>
   );
